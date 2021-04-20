8ca7c73f9c18deffdfb32c6ed9ab647ac9545b53
Brian Cloud <brian.cloud98@gmail.com>
2021-04-19 22:11:24 -0500

    Updated Frogger

---
+++ Frogger/Assets/Car.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float minSpeed = 8f;
+    public float maxSpeed = 12f;
+
+    float speed = 1f;
+
+    private void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+}
